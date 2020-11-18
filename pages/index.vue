b081baecece370baeb8f50ba699d49f441a2e3b7
İbrahim Can Karta <ibrahimcankarta@gmail.com>
2020-11-19 00:12:09 +0300

    position-fixed

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,39 @@
-<template>
-  <div class="container my-4 w-full">
-    <div class="flex flex-col items-center justify-center">
-      <div class="bg-green-500 w-32 h-32 rounded-full"></div>
-      <h1 class="text-2xl font-bold">Muğla Sıtkı Koçman Üniversitesi</h1>
-      <h1 class="my-2 text-sm">Developers Student Club</h1>
-      <button class="mt-4 bg-red-500 text-white px-4 py-2 rounded-lg w-full">
-        Github Etkinliği
+<template  >
+
+  <div class=" bg-gradient-to-t container my-4 w-full">
+    <div class=" bg-gradient-to-t div-head flex flex-col items-center justify-center">
+      
+      <div class="bg-blue-500 px-6 py-2 rounded-xl mt-4 w-70 h-55 "> <img class="h-48 w-full object-cover rounded-full" src="gdsc-logo.png" alt="logo"></div>
+    
+      <button  class="btn-github mt-6 mb-6 text-3xl bg-purple-700 text-white px-6 py-2 rounded-2xl w-90  ">
+        Next Event
       </button>
-      <button class="mt-4 bg-red-850 text-white px-4 py-2 rounded-lg w-full">
-        <span class="mdi mdi-youtube"></span> Youtube
+      <div class="inline">
+
+    
+      <button class=" inline btn-youtube mt-4 bg-red-700 text-white px-6 py-2 rounded-lg">
+        <span class="mdi w-full rounded-lg mdi-youtube"></span> 
       </button>
-      <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg w-full">
-        <span class="mdi mdi-twitter"></span> Twitter
+      <button class="btn-twitter inline mt-4 bg-blue-500 text-white px-6 py-2 rounded-lg ">
+        <span class="mdi mdi-twitter"></span> 
       </button>
-      <button class="mt-4 bg-blue-600 text-white px-4 py-2 rounded-lg w-full">
-        <span class="mdi mdi-facebook"></span> Facebook
+      <button class="btn-ig inline-block mt-4 mb-6 bg-red-600 text-white px-6 py-2 rounded-lg ">
+        <span class="mdi mdi-instagram"></span> 
       </button>
-      <button class="mt-4 bg-green-600 text-white px-4 py-2 rounded-lg w-full">
-        <span class="mdi mdi-whatsapp"></span> Whatsapp
+      <div class=" block-flex justify-center items-center" >
+        <button class=" inline btn-whatsapp mt-4 bg-green-600 text-white px-6 py-2 rounded-lg ">
+        <span class=" inline mdi mdi-whatsapp"></span> 
       </button>
-      <button class="mt-4 bg-blue-400 text-white px-4 py-2 rounded-lg w-full">
-        <span class="mdi mdi-discord"></span> Discord
+      <button class="btn-discord mt-4 bg-blue-400 text-white px-6 py-2 rounded-lg ">
+        <span class="mdi mdi-discord"></span> 
       </button>
+      <button class="btn-github mt-4 bg-gray-600 text-white px-6 py-2 rounded-lg ">
+        <span class="mdi mdi-git "></span> 
+      </button>
+      </div>
+      
+      </div>
+     
     </div>
   </div>
 </template>
@@ -39,4 +51,27 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+
+
+.btn-github:hover{
+  @apply bg-gray-800;
+}
+.btn-youtube:hover{
+  @apply bg-gray-800;
+}
+.btn-twitter:hover{
+  @apply bg-gray-800;
+}
+.btn-ig:hover{
+  @apply bg-gray-800;
+}
+.btn-whatsapp:hover{
+  @apply bg-gray-800;
+}
+.btn-discord:hover{
+  @apply bg-gray-800;
+}
+
+
+</style>
